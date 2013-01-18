666144e6d58f6df13094e5b37fce2f0579b05733
± ryan <ryan@2sidedfigure.net>
2013-01-18 00:49:49 -0800

    Add example demonstrating use of multiple methods

---
+++ examples/multiple/index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var
+
+// required modules
+express = require('express'),
+consolidate = require('consolidate'),
+
+// exstatic configuration object
+conf = {
+    cachePath: __dirname + '/static_cache',
+    version: 'v0.0.0',
+    compress: true //could be based on NODE_ENV or a force compression flag
+},
+
+// load and configure exstatic
+// normally, require('exstatic')(conf)
+exstatic = require('../../lib/StaticAssetManager')(conf),
+
+// register the static asset handler
+sa1 = exstatic.createAsset(
+    'Png', // the type of asset we're working with
+    { // the settings object
+        id: 'img_test',
+        pattern: '/static/:version/:cacheId/img/test.png', // the path pattern to use
+        files: [ // a list of the assets to include in the bundle
+            '../png/assets/test.png'
+        ]
+    }
+),
+
+sa2 = exstatic.createAsset(
+    'Stylus',
+    {
+        id: 'css_style',
+        pattern: '/static/:version/:cacheId/css/style.css',
+        files: [
+            'assets/style.stylus'
+        ]
+    }
+),
+
+// get an express application
+app = express();
+
+// set up the view engine
+app.engine('html', consolidate.hogan);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// gzip the output
+app.use(express.compress());
+
+// use the exstatic middleware
+app.use(exstatic.middleware);
+
+// handler for the index
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// start the server
+app.listen(8888);
+
+// exstatic should print a debug line for indicating the URL of the registered asset.
+// have a look by running:
+//
+// $ curl localhost:8080/<url_from_debug>
